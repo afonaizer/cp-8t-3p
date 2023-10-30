c9fa7c127129161372988486e86c0468f7979c5d
afonaizer <123896092+afonaizer@users.noreply.github.com>
2023-10-30 19:23:46 +0200

    Add files via upload

---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        ////задан1
+        //static void Hashtags()
+        //{
+        //    for (int a = 0; a < 4; a++) Console.WriteLine();
+        //    for (int b = 0; b < 20; b++) Console.Write("#");
+        //}
+        //static void Main(string[] args)
+        //{
+        //    Hashtags();
+        //    Console.ReadKey();
+        //}
+
+
+        ////задан2
+        //static void Triangle()
+        //{
+        //    int x = 8;
+        //    for (int a = 0; a < 8; a++)
+        //    {
+        //        for (int b = 0; b < 8; b++)
+        //        {
+        //            if (b == 8) Console.Write("#");
+        //            if (b < x) Console.Write("#");
+        //        }
+        //        Console.Write("\n");
+        //        x--;
+        //    }
+        //    Console.ReadKey();
+        //}
+        //static void Main(string[] args)
+        //{ 
+        //    Triangle(); 
+        //}
+
+
+        //задан3
+        //static void Equation()
+        //{
+        //    double x = ((Math.Sqrt(5) + 5) / (Math.Sqrt(7) + 7)) + ((Math.Sqrt(12) + 12) / (Math.Sqrt(8) + 8)) + ((Math.Sqrt(19) + 19) / (Math.Sqrt(2) + 2));
+        //    Console.WriteLine($"{x}");
+        //}
+        //static void Main(string[] args)
+        //{
+        //    Equation();
+        //    Console.ReadKey();
+        //}
+
+        //задан4
+        //static void Max()
+        //{
+        //    int[] abcd = new int[4];
+        //    Console.Write("Введите число a: ");
+        //    abcd[0] = int.Parse(Console.ReadLine());
+        //    Console.Write("Введите число b: ");
+        //    abcd[1] = int.Parse(Console.ReadLine());
+        //    Console.Write("Введите число с: ");
+        //    abcd[2] = int.Parse(Console.ReadLine());
+        //    Console.Write("Введите число d: ");
+        //    abcd[3] = int.Parse(Console.ReadLine());
+        //    int max = abcd.Max();
+        //    Console.WriteLine($"Наибольшим значением является {max}.");
+        //}
+        //static void Main(string[] args)
+        //{
+        //Max();
+        //Console.ReadKey();
+        //}
+    }
+}
